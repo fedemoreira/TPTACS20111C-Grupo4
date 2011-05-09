c102499a732e4f1ec7d7efb0cd77be56c71c8c3c
Ignacio Coluccio <elpatointerior@hotmail.com>
2011-05-10 00:47:09 +0800

    Arreglado test

--- src/main/java/grupo4/tests/HttpClient.java
+++ src/main/java/grupo4/tests/HttpClient.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import main.java.grupo4.clientes.ClienteServiciosREST;
 import main.java.grupo4.exceptions.ImposibleConsumirException;
 
+import org.apache.http.ProtocolVersion;
 import org.apache.http.message.BasicHttpResponse;
 import org.apache.http.message.BasicStatusLine;
 import org.junit.Test;
@@ -13,6 +14,6 @@ public class HttpClient {
 	@Test(expected=ImposibleConsumirException.class)
 	public void testBasico() throws IOException
 	{
-		new ClienteServiciosREST().obtenerTextoDeRespuesta(new BasicHttpResponse(new BasicStatusLine(null, 404, "Mock")));
+		new ClienteServiciosREST().obtenerTextoDeRespuesta(new BasicHttpResponse(new BasicStatusLine(new ProtocolVersion("", 0, 0), 404, "Mock")));
 	}
 }
