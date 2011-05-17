60b8551e17253bf2abb03ef580f878a403efcad0
Ignacio Coluccio <elpatointerior@hotmail.com>
2011-05-16 22:59:25 -0300

    Muestra categorías + refactor

--- src/main/java/grupo4/servlets/ServletMercadoLibre.java
+++ src/main/java/grupo4/servlets/ServletMercadoLibre.java
@@ -1,7 +1,9 @@
 package main.java.grupo4.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
@@ -10,47 +12,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
 import main.java.grupo4.clientes.ClienteServiciosREST;
 
 /**
  * Servlet implementation class ServletMercadoLibre
  */
-public class ServletMercadoLibre extends HttpServlet implements Servlet {
+public class ServletMercadoLibre extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ClienteServiciosREST clienteRest;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletMercadoLibre() {
-        super();
-        this.clienteRest = new ClienteServiciosREST();
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletMercadoLibre() {
+		super();
+		this.clienteRest = new ClienteServiciosREST();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	 	response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
 
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Grupo 4</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println(this.clienteRest.pedir("https://api.mercadolibre.com/sites/MLA/search?q=cafetera"));
-        out.println("</body>");
-        out.println("</html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Grupo 4</title>");
+		out.println("</head>");
+		out.println("<body>");
 
-        out.close();
+
+		for(Object jso :  this.clienteRest.obtenerArrayJSON("[" + this.clienteRest.pedir("https://api.mercadolibre.com/sites/MLA/categories") + "]"))
+			out.println("Categoría: " + jso + "\n\n");
+			
+		out.println("</body>");
+		out.println("</html>");
+
+		out.close();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
 }
