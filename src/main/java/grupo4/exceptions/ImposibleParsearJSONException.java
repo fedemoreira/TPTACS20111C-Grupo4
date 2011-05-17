b6558023e307183d277a10a44f6a975797279f73
Ignacio Coluccio <elpatointerior@hotmail.com>
2011-05-17 11:02:07 -0300

    Refactor, separado el ParserJSON y sus tests

---
+++ src/main/java/grupo4/exceptions/ImposibleParsearJSONException.java
@@ -0,0 +1,13 @@
+package main.java.grupo4.exceptions;
+
+public class ImposibleParsearJSONException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5068290185557975617L;
+
+	public ImposibleParsearJSONException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/grupo4/ParserJSONTest.java
@@ -0,0 +1,34 @@
+package test.java.grupo4;
+
+import static org.junit.Assert.assertEquals;
+import main.java.grupo4.clientes.ParserJSON;
+import main.java.grupo4.exceptions.ImposibleParsearJSONException;
+
+import org.json.simple.parser.JSONParser;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserJSONTest {
+
+	private ParserJSON parser;
+	private String jsonstring;
+
+	@Before
+	public void condicionesIniciales()
+	{
+		this.parser = new ParserJSON();
+		this.jsonstring = "{\"id\":36484980,\"power_seller_status\":\"platinum\",\"car_dealer\":false,\"real_estate_agency\":false}";
+	}
+	@Test
+	public void testObtenerCampoJson() throws org.json.simple.parser.ParseException
+	{
+		assertEquals("36484980", this.parser.obtenerValorDeCampoJSON(this.jsonstring, "id"));
+	}
+	
+	@Test(expected=ImposibleParsearJSONException.class)
+	public void testObtenerCampoJsonImposible() throws org.json.simple.parser.ParseException
+	{
+		assertEquals("36484980", this.parser.obtenerValorDeCampoJSON(this.jsonstring, "noexiste"));
+	}
+	
+}
