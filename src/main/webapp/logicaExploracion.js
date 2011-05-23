f6a47a53a68463823dbd6fc4a5895fc12ef60c77
Ignacio Coluccio <elpatointerior@hotmail.com>
2011-05-23 17:29:03 +0000

    Muestra primera página de productos de cada categoría.

--- src/main/webapp/logicaExploracion.js
+++ src/main/webapp/logicaExploracion.js
@@ -1,46 +1,54 @@
-
 var regenerarPathToRoot = function(datos)
 {
-        $.each(datos.path_from_root, function(key, data)
-        {
-          $("<div class=\'cat\' idCat=\'" + data.id + "\'>" + data.name +  "</div>").appendTo('.pathToRoot');
-        });
+    $.each(datos.path_from_root, function(key, data)
+           {
+               $("<div class=\'cat\' idCat=\'" + data.id + "\'>" + data.name +  "</div>").appendTo('.pathToRoot');
+           });
 }
 
 var regenerarCategorias = function(data)
 {
-  var items = [];
+    $.each(data, function(key, val) {
+	$("<div class=\"cat\" idCat=\"" + val.id + "\">" + val.name + "</div>").appendTo('.listaDeCategorias');
+    });
+}
 
-  $.each(data, function(key, val) {
-    $("<div class=\"cat\" idCat=\"" + val.id + "\">" + val.name + "</div>").appendTo('.listaDeCategorias');
-  });
+var regenerarProductos = function(data)
+{
+    $.each(data, function(key, val) {
+	$("<div class=\"producto\" idProd=\"" + val.id + "\"> " + "<a href=\"" + val.permalink + "\">"  + val.title + " $" + val.price +  "</a> </div>").appendTo('.productos');
+    });
 }
 
 var obtenerCategorias = function(datos)
 {	
-  var items = [];
-  $(".pathToRoot").empty();
-  regenerarCategorias(datos.children_categories);
-  regenerarPathToRoot(datos);
+    var items = [];
+    $(".pathToRoot").empty();
+    regenerarCategorias(datos.children_categories);
+    regenerarPathToRoot(datos);
 };
 
 var limpiar = function()
 {
-        $(".pathToRoot").empty();
-	$(".listaDeCategorias").empty();
+    $(".pathToRoot").empty();
+    $(".listaDeCategorias").empty();
+    $(".productos").empty();
 }
 
 
 
 // Carga inicial
 $.getJSON("ServletMercadoLibre", function(data) {
-regenerarCategorias(data);
+    regenerarCategorias(data);
 });
 
 
 $(".cat").live('click', function() {
-        limpiar();
-	$.getJSON("ServletMercadoLibre?cat=" + $(this).attr("idCat"), function(datos) {
-		obtenerCategorias(datos);
-		});	
-	});
+    limpiar();
+    $.getJSON("ServletMercadoLibre?cat=" + $(this).attr("idCat"), function(datos) {
+	obtenerCategorias(datos);
+    });	
+    $.getJSON("ServletMercadoLibre?cat=" + $(this).attr("idCat") + "&productos=true", function(datos) {
+	regenerarProductos(datos.results);
+    });	
+});
