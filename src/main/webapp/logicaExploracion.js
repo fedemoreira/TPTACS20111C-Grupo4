aff96255667b443ce9a9dbbc9a7f91414ece6ff8
Ignacio Coluccio <elpatointerior@hotmail.com>
2011-06-06 20:05:41 -0300

    Cambiado estilo

--- src/main/webapp/logicaExploracion.js
+++ src/main/webapp/logicaExploracion.js
@@ -1,89 +1,55 @@
 const direccionDelServlet = "ServletMercadoLibre";
 
-var appendDivAListaCategorias = function(id, name, divDondeAgregar,sangria)
+var appendDivAListaCategorias = function(id, name, divDondeAgregar)
 {
 	$("<div class=\'cat\' idCat=\'" + id + "\'>" + name + "<div style= </div>").appendTo(divDondeAgregar);
 };
 
 var regenerarPathToRoot = function(datos)
-{   var numeroCatPadres = 0; 
-    $(".tituloPathToRoot").show();
-    $("<div class=\'root\'\'> Home </div>").appendTo('.pathToRoot');
+{   
+    $(".pathToRoot").toggle("highlight", 400);
+    $(".pathToRoot").empty();
     $.each(datos.path_from_root, function(key, data) {
-    	appendDivAListaCategorias(data.id, data.name, '.pathToRoot',numeroCatPadres*10);
-		numeroCatPadres += 1;
+    	appendDivAListaCategorias(data.id, data.name, '.pathToRoot');
     });
+    $(".pathToRoot").toggle("highlight", 400);
 };
 
 var regenerarCategorias = function(data)
 {
-    $(".tituloCategorias").show();
     $.each(data, function(key, val) {
 		appendDivAListaCategorias(val.id, val.name, '.listaDeCategorias');
     });
 };
 
-var limpiarMenuProductos = function()
-{   var i = 0;
-	for (i = (document.menu.productos2.options.length-1); i >= 0; i -=1)
-		{document.menu.productos2.options[i]= null;};
-}
-
 var regenerarProductos = function(data)
 {
-	
-        $(".tituloProductos").hide();
-        $(".productos").hide();
-        limpiarMenuProductos();
         $.each(data, function(key, val) {
 		$("<li> <div class=\"producto\" idProd=\"" + val.id + "\"> " + 
 		"<a href=\"" + val.permalink + "\">"  + "$" + val.price 
 		+ " -  " +  val.title +  "</a> </div></li>").appendTo('.productos');
-		nuevaopcion = new Option("$" + val.price + " -  " +  val.title ,val.permalink);  
-		document.menu.productos2.options[document.menu.productos2.options.length]= nuevaopcion;
     });
 };
 
-var obtenerCategorias = function(datos)
-{	
-    var items = [];
-    $(".pathToRoot").empty();
-    regenerarCategorias(datos.children_categories);
-    regenerarPathToRoot(datos);
-};
 
-var ocultarTitulos = function()
-{
-    $(".tituloProductos").hide();
-    $(".tituloCategorias").hide();
-    $(".tituloPathToRoot").hide();
-};
-
-var limpiar = function()
-{
-    $(".pathToRoot").empty();
-    $(".listaDeCategorias").empty();
+$(".cat").live('click', function() {   
+    
     $(".productos").empty();
-    ocultarTitulos();
-};
-
-$(".cat").live('click', function() {
-    limpiar();
+    $(".listaDeCategorias").toggle("highlight", 400);
+    $(".listaDeCategorias").empty();
     var idCategoria = $(this).attr("idCat");
 	$.getJSON("https://api.mercadolibre.com/categories/" + idCategoria + "?callback=?", function(data) {
-		obtenerCategorias(data[2]);
+		regenerarCategorias(data[2].children_categories);
+		regenerarPathToRoot(data[2]);
 	});
+	$(".listaDeCategorias").toggle("highlight", 400);
+	
+	
+    
 	$.getJSON("https://api.mercadolibre.com/sites/MLA/search?category=" +
 			  idCategoria + "&callback=?", function(data) {
 				regenerarProductos(data[2].results);
 	});
-});
-
-$(".root").live('click', function() {
-    limpiar();
-	$.getJSON("https://api.mercadolibre.com/sites/MLA/categories?callback=?", function(data) {
-		regenerarCategorias(data[2]);
-		});
 });
 
 var mostrarSinResultados = function()
@@ -106,7 +72,8 @@ $.getJSON("https://api.mercadolibre.com/sites/MLA/search?q=" +
 }
 
 $('#busqueda').live('submit', function() {
-	limpiar();
+	$(".productos").empty();
+	$(".cat").remove();
     busqueda($("input:first").val());
 	return false;
 	}); 
@@ -118,17 +85,12 @@ $('#volverAlIndice').live('submit', function() {
 });	 
 
 
-$('#ir').live('submit', function() {
-open(document.menu.productos2.options[document.menu.productos2selectedIndex].value);
-	
-});
-
-
 // Carga inicial
 $(document).ready(function(){
-	ocultarTitulos();
+	
 	$.getJSON("https://api.mercadolibre.com/sites/MLA/categories?callback=?", function(data) {
 		regenerarCategorias(data[2]);
 	});
+	
 });
 
