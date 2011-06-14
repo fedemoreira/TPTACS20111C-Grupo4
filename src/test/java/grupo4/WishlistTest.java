332e865ed1de2633214047fd09ec556a41982423
Ignacio Coluccio <elpatointerior@hotmail.com>
2011-06-14 14:11:11 +0000

    Cambiado string por un objeto Producto

--- src/test/java/grupo4/WishlistTest.java
+++ src/test/java/grupo4/WishlistTest.java
@@ -1,17 +1,9 @@
 package grupo4;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import grupo4.Wishlist.Producto;
 import grupo4.Wishlist.Wishlist;
 
-
-import java.io.UnsupportedEncodingException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.ProtocolVersion;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHttpResponse;
-import org.apache.http.message.BasicStatusLine;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,27 +11,25 @@ public class WishlistTest {
 	
 
 	private Wishlist wishlist;
-    private String stringmocked = "esta es una respuesta";
-    
+    private Producto productoEjemplo;
 	@Before
-	public void condicionesIniciales() throws UnsupportedEncodingException
+	public void condicionesIniciales()
 	{
 		this.wishlist = new Wishlist();
-
+		this.productoEjemplo = new Producto("Producto");
 	}
 	
 	@Test
 	public void agregayQuitaBienUnProducto()
 	{   
-		this.wishlist.aniadirProducto(this.stringmocked); 
-		assertTrue(this.wishlist.quitarProducto(this.stringmocked));
-		
+		this.wishlist.aniadirProducto(this.productoEjemplo); 
+		this.wishlist.quitarProducto(this.productoEjemplo);
 	}	
 	@Test
 	public void agregaySacaBienUnProducto()
 	{   
-		this.wishlist.aniadirProducto(this.stringmocked); 
-		assertTrue(this.wishlist.sacameUnProducto()== this.stringmocked );
+		this.wishlist.aniadirProducto(this.productoEjemplo); 
+		assertTrue(this.wishlist.sacameUnProducto()== this.productoEjemplo );
 	}
 		
 }
